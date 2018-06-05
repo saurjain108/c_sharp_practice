7a928609015b3384090137212e5b211774b13b83
raunak jain <rony.jain108@gmail.com>
2018-06-05 08:28:49 -0500

    Introduction

---
+++ practice-1/practice-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string userinput = Console.ReadLine();
+
+            int number1 = int.Parse(userinput);
+
+            Console.Write("Enter the second number: ");
+            userinput = Console.ReadLine();
+
+            int number2 = int.Parse(userinput);
+
+            int sum = number1 + number2;
+            Console.WriteLine("The sum is " + sum);
+            Console.WriteLine("The sum is " + sum.ToString());
+            Console.WriteLine("The sum is " + (number1 + number2));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
