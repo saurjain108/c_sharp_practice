a7eab7bbd21142b657df582921bd4fcb25219a20
raunak jain <rony.jain108@gmail.com>
2018-06-05 22:33:11 -0500

    introduction-2

---
+++ introduction2/introduction2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.Write("Enter your name:  ");
+        string x = Console.ReadLine();
+        Console.WriteLine( x + ", you're exiting the terminal");
+        Console.WriteLine("Have a good day {0}", x);
+        Console.ReadLine();
+    }
+    }
