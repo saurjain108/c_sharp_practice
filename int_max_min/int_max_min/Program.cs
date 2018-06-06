8252cc4d82d5d37e13194c8b03d06c586cf9cee6
raunak jain <rony.jain108@gmail.com>
2018-06-06 00:15:37 -0500

    checking the maximum value that integer can hold.

---
+++ int_max_min/int_max_min/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        int x = 0;
+
+        Console.WriteLine("The minimum value is {0}", int.MinValue);
+        Console.WriteLine("The maximum value is {0}", int.MaxValue);
+        Console.WriteLine("press enter to Exit");
+        Console.ReadLine();
+
+    }
+}
+
