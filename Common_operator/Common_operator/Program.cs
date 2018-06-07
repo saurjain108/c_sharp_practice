8729769b5817256cc3d14fd4820112747b082dd8
raunak jain <rony.jain108@gmail.com>
2018-06-06 21:46:23 -0500

    This is with another function inside main function

---
+++ Common_operator/Common_operator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+     int i, j;
+
+        Console.WriteLine("Print the first number: ");
+        i = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("print the second number: ");
+        j= int.Parse(Console.ReadLine());
+
+///     Console.ReadLine();
+
+        Console.WriteLine("the sum of these two numbers is "+ (i + j));
+        Console.WriteLine("the sub of these two numbers is " + (i - j));
+        Console.WriteLine("the product of these two numbers is " + (i * j));
+        Console.WriteLine("the division of these two numbers is "+ (i / j));
+
+        Console.ReadLine();
+        one();
+        }
+static void one()
+    {
+        int number = 21;
+
+        bool num = number == 19 ? true : false;
+
+        Console.WriteLine("number is {0}", number);
+        Console.ReadLine();
+            }
+
+   }
+
