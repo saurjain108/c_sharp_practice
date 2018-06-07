40470c567ce0656fcbadedbdc1a61fb9bf11a580
raunak jain <rony.jain108@gmail.com>
2018-06-06 20:56:28 -0500

    datatype

---
+++ getting_to_know_datatypes/getting_to_know_datatypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        string Fruit = "\"Apple\"";
+        Console.WriteLine(Fruit);
+        Console.ReadLine();
+        newline();
+
+
+        void newline()
+        {
+            string Newline = "One\nTwo\nThree";
+            Console.WriteLine(Newline);
+            Console.ReadLine();
+
+        }
+
+
+
+        }
+    }
