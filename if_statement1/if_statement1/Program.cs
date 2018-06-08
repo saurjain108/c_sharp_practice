eb92321d9c8d40db5bbea4972cb74cfb2707a66b
raunak jain <rony.jain108@gmail.com>
2018-06-07 23:03:22 -0500

    if-else conditions

---
+++ if_statement1/if_statement1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Enter a number");
+        int i = int.Parse(Console.ReadLine());
+
+        if(i ==1)
+        {
+            Console.WriteLine("this is number {0}", i);
+            Console.ReadLine();
+        }
+ else   if (i ==2 )
+        {
+            Console.WriteLine("this is number {0}", i);
+            Console.ReadLine();
+        }
+
+ else   if (i == 3)
+        {
+            Console.WriteLine("this is number {0}", i);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Try either 1 or 2 or 3"); 
+            Console.ReadLine();
+        }
+        }
+    }
