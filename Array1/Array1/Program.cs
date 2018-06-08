be62b08c2e5edde6b1143796322a16485051d5db
raunak jain <rony.jain108@gmail.com>
2018-06-07 22:32:10 -0500

    Array1

---
+++ Array1/Array1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int[] numbers = new int[1000];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+      numbers[3] = 3;
+
+
+        Console.WriteLine(numbers[3]);
+        Console.ReadLine();
+
+
+    }
+}
