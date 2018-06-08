0b8bb7970b2ddb5336fb41061879cf33ef42e103
raunak jain <rony.jain108@gmail.com>
2018-06-07 23:15:01 -0500

    knowing the switch and break statements

---
+++ switch-break-statement/switch-break-statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number");
+        int i = int.Parse(Console.ReadLine());
+        switch (i)
+        {
+            case 10:
+                Console.WriteLine("your number is 10");
+                Console.ReadLine();
+                break;
+            case 20:
+                Console.WriteLine("your number is 20");
+                Console.ReadLine();
+                break;
+            case 30:
+                Console.WriteLine("your number is 30");
+                Console.ReadLine();
+                break;
+            default:
+                Console.WriteLine("your number is not 10 ,20 or 30");
+                Console.ReadLine();
+                break; 
+
+
+        }
+
+        
+       
+    }
+}
