01ad3490f792c9605e1f76f5618c51ebaea64bb4
raunak jain <rony.jain108@gmail.com>
2018-06-13 00:54:02 -0500

    if-else/if-else condition

---
+++ switch2/switch2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter the color: ");
+        string color = Console.ReadLine();
+        //Console.ReadLine();
+        if (color == "red")
+        {
+            Console.WriteLine("your favorite color is Red");
+            Console.ReadLine();
+
+        }
+
+        else if (color == "pink")
+        {
+            Console.WriteLine("your favorite color is pink");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("you do not have favourite color");
+            Console.ReadLine();
+        }
+        }
+    }
